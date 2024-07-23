f86c2c282e1fe112769f4ea91878cd18e4ff630a
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2024-07-23 17:40:06 -0600

    Se agrego el front al back en backNotas y se subio a produccion

--- Part3/BackNotas/index.js
+++ Part3/BackNotas/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -33,6 +34,8 @@ const requestLogger = (req, res, next) => {
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
+app.use(express.static("dist"));
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "Unknown endpoint" });
@@ -94,7 +97,7 @@ app.post("/api/notes", (req, res) => {
 app.use(unknownEndpoint);
 
 //Escucho por consola y selecciono el puerto
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Instalacion de nodemon para backend y escuchar los cambios y poder reflejarlos e
 
 npm install --save-dev nodemon
 y agregamos en package.json en sripts= "dev": "nodemon index.js",
+
+para llevar los cambios a fly.io a produccion ejecutar
+fly deploy
