a281e18c5774ea552eee4da84dce8dce4dadd5db
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2024-08-17 17:43:09 -0600

    Se agrego rutas

--- Parte7/PrimerContactoRouter/src/App.jsx
+++ Parte7/PrimerContactoRouter/src/App.jsx
@@ -1,35 +1,165 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  //useParams,
+  useNavigate,
+  Navigate,
+  useMatch,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+      ducimus repellendus illo, nam pariatur omnis harum architecto dolorum
+      tempore culpa quia itaque temporibus ipsam aperiam totam. Consequatur
+      ullam earum perferendis.
+    </p>
+  </div>
+);
+
+const Note = ({ note }) => {
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? "important" : ""} </strong>
+      </div>
+    </div>
+  );
+};
+
+const Notes = ({ notes }) => {
+  return (
+    <div>
+      <h2>Notes</h2>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <Link to={`/notes/${note.id}`}>{note.content} </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Users = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
+  </div>
+);
+
+const Login = (props) => {
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    props.onLogin("mluukkai");
+    navigate("/");
+  };
 
   return (
-    <>
+    <div>
+      <h2>login</h2>
+
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type="password" />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 3,
+      content: "Most important methods of HTTP-protocol are GET and POST",
+      important: true,
+      user: "Arto Hellas",
+    },
+  ]);
+  const [user, setUser] = useState(null);
+
+  const login = (user) => {
+    setUser(user);
+  };
+
+  const padding = {
+    padding: 5,
+  };
+
+  const match = useMatch("/notes/:id");
+  const note = match
+    ? notes.find((note) => note.id === Number(match.params.id))
+    : null;
+
+  return (
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Link style={padding} to={"/"}>
+          home
+        </Link>
+        <Link style={padding} to={"/notes"}>
+          notes
+        </Link>
+        <Link style={padding} to={"/users"}>
+          users
+        </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to={"login"}>
+            login
+          </Link>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <Routes>
+        <Route path="/notes" element={<Notes notes={notes} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/notes/:id" element={<Note note={note} />} />
+        <Route path="/login" element={<Login onLogin={login} />} />
+        <Route
+          path="/users"
+          element={user ? <Users /> : <Navigate replace to={"/login"} />}
+        />
+      </Routes>
+
+      <div>
+        <br />
+        <i>Note app, Departament of Computer Science 2024</i>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
